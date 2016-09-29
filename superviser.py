94b109e3431e55bcd533bbe4c19578f5c5b54b06
Kishlin <legrand.pierrelouis@gmail.com>
2016-09-29 14:07:58 +0200

    Code formating + comments.

--- superviser.py
+++ superviser.py
@@ -6,50 +6,78 @@ import time
 import logging
 import argparse
 
-def Ajoutdossier(nom):
-    logging.info("le dossier " + nom +" a été ajouté")
 
-def Suppressiondossier(nom):
-    logging.info("le dossier "+nom+" a été supprimé")
+# Annonce la création d'un dossier.
+# @param name nom du dossier créé
+def folderAdded(name):
+    logging.info("The folder " + name + " has been added")
 
-def Modification(nom):
-    logging.info("le fichier "+nom+" a été modifié")
+# Annonce la supression d'un dossier.
+# @param name nom du dossier supprimé
+def folderDeleted(name):
+    logging.info("The folder " + name + " has been deleted")
 
-def Ajoutfichier(nom):
-    logging.info("le fichier "+nom+" a été ajouté")
+# Annonce la modification d'un fichier.
+# @param name nom du fichier modifié
+def fileModified(name):
+    logging.info("The file " + name + " has been modified")
 
-def Suppressionfichier(nom):
-    logging.info("le fichier "+nom+" a été supprimé")
+# Annonce la création d'un ficher
+# @param name nom du fichier créé
+def fileAdded(name):
+    logging.info("The file " + name + " has been added")
 
-def Analysedossier(ancien, nouveau):
-    for path in nouveau.keys():
-        if path in ancien.keys():
-            Comparerfichier(ancien[path], nouveau[path])
-        else :
-            Ajoutdossier(path)
-    for path in ancien.keys():
-        if path not in nouveau.keys():
-            Suppressiondossier(path)
+# Annonce la suppression d'un fichier
+# @param name nom du fichier supprimé
+def fileDeleted(name):
+    logging.info("The file " + name + " has been deleted")
 
-def Comparerfichier(ancienpath, nouveaupath):
-    for nom in nouveaupath.keys():
-        if nom in ancienpath.keys():
-            if nouveaupath[nom] != ancienpath[nom]:
-                Modification(nom)
-        else :
-            Ajoutfichier(nom)
-    for nom in ancienpath.keys():
-        if nom not in nouveaupath.keys():
-            Suppressionfichier(nom)
+# Compare deux dictionnaires de dossiers pour en trouver les différences
+# @param old ancien état
+# @param new nouvel état
+def folderAnalyse(old, new):
+    for path in new.keys():
+        if path in old.keys():
+            compareFiles(old[path], new[path])
+        else:
+            folderAdded(path)
+    for path in old.keys():
+        if path not in new.keys():
+            folderDeleted(path)
 
+# Compare deux dictionnaires de fichiers pour en trouver les différences
+# @param oldPath ancier état
+# @param newPath nouvel état
+def compareFiles(oldPath, newPath):
+    for name in newPath.keys():
+        if name in oldPath.keys():
+            if newPath[name] != oldPath[name]:
+                fileModified(name)
+        else:
+            fileAdded(name)
+    for nom in oldPath.keys():
+        if nom not in newPath.keys():
+            fileDeleted(nom)
+
+# Remplie un dictionnaire de fichiers sour la forme "nom" -> timestamp dernière édition
+# @param dictionary dictionnaire de fichiers à compléter
+# @param folder nom du dossier contenant le fichier
+# @param files liste des fichiers à mettre dans le dictionnaire
 def fill_files_dictionary(dictionary, folder, files) :
     for file in files :
         full_path = os.path.join(folder, file)
+        # Récupération du timestamp de dernière modification
         timestamp = int(os.path.getmtime(full_path))
         dictionary[file] = timestamp
 
+# Remplie un dictionnaire de dossiers sous la forme "nom" -> dictionnaire des fichiers contenus
+# @param dictionary dictionnaire de dossiers à remplir
+# @param folder dossier source
+# @param step_init échelon de sous-dossier initial
+# @param step_max échelon de sous-dossier maximal
 def fill_directories_dictionary(dictionary, folder, step_init, step_max) :
     for root, dirs, files in os.walk(folder):
+        # Si on est au niveau de récursivité maximal, on vide la liste des sous dossiers pour stoper le walk
         if root.count(str(os.sep)) >= step_max + step_init and step_max != 0 :
             del dirs[:]
         files_dictionary = {}
@@ -66,7 +94,7 @@ def run(args) :
         time.sleep(args.time)
         new_state = {}
         fill_directories_dictionary(new_state, args.directory, 0, args.recursive)
-        Analysedossier(old_state, new_state)
+        folderAnalyse(old_state, new_state)
         old_state = new_state
 
 
